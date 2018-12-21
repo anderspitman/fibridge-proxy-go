fd3e48043eff1f08a2ab27a504e474e78d67652b
Anders Pitman <tapitman11@gmail.com>
2018-12-21 14:57:25 -0700

    Move stream handling in from omniconc

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
         "fmt"
         "log"
         "net/http"
+        "github.com/anderspitman/omnistreams-core-go"
         "github.com/anderspitman/omnistreams-concurrent-go"
 )
 
@@ -12,6 +13,26 @@ func main() {
 	fmt.Printf("Hi there\n")
 
         muxAcceptor := omniconc.CreateWebSocketMuxAcceptor()
+
+        muxAcceptor.OnMux(func(mux *omniconc.Multiplexer) {
+                log.Println("got muxy")
+                mux.OnStream(func(stream omnicore.Producer) {
+
+                        log.Println("Got a streamy")
+
+                        stream.OnData(func(data []byte) {
+                                log.Println(len(data))
+                                stream.Request(1)
+                        })
+
+                        stream.OnEnd(func() {
+                                log.Println("end streamy")
+                                mux.SendControlMessage([]byte("hi there"))
+                        })
+
+                        stream.Request(10)
+                })
+        })
 
         http.HandleFunc("/", muxAcceptor.GetHttpHandler())
         log.Fatal(http.ListenAndServe("localhost:9001", nil))

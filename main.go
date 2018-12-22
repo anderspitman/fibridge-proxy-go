b943a1ccb45c9c1abaa3d0ed84de0686d234eec3
Anders Pitman <tapitman11@gmail.com>
2018-12-21 17:15:10 -0700

    Implement passing stream to http client

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
         "fmt"
         "log"
         "net/http"
+        "strings"
         omnicore "github.com/anderspitman/omnistreams-core-go"
         omniconc "github.com/anderspitman/omnistreams-concurrent-go"
         "github.com/satori/go.uuid"
@@ -15,6 +16,12 @@ func main() {
 
         muxes := make(map[uuid.UUID]*omniconc.Multiplexer)
 
+        // TODO: use uint32
+        var nextRequestId byte
+        nextRequestId = 0
+
+        streamChannels := make(map[byte]chan(omnicore.Producer))
+
         muxAcceptor := omniconc.CreateWebSocketMuxAcceptor()
 
         muxAcceptor.OnMux(func(mux *omniconc.Multiplexer) {
@@ -22,33 +29,77 @@ func main() {
 
                 id := uuid.Must(uuid.NewV4())
 
-                log.Println(id)
+                fmt.Printf("curl localhost:9001/%s", id)
 
                 // TODO: need to delete muxes after connection closes
                 muxes[id] = mux
 
+                var idForNextStream byte
+
                 mux.OnControlMessage(func(message []byte) {
                         log.Println("Control message:", message)
+                        idForNextStream = message[0]
                 })
 
                 mux.OnStream(func(stream omnicore.Producer) {
 
-                        log.Println("Got a streamy")
+                        log.Println("Got a streamy, use id", idForNextStream)
 
-                        stream.OnData(func(data []byte) {
-                                log.Println(len(data))
-                                stream.Request(1)
-                        })
+                        streamChannel := streamChannels[idForNextStream]
 
-                        stream.OnEnd(func() {
-                                log.Println("end streamy")
-                                mux.SendControlMessage([]byte("all done"))
-                        })
-
-                        stream.Request(10)
+                        if streamChannel != nil {
+                                streamChannel <- stream
+                        }
                 })
         })
 
-        http.HandleFunc("/", muxAcceptor.GetHttpHandler())
+        var httpHandler = func(w http.ResponseWriter, r *http.Request) {
+                if r.Method == "GET" {
+                        id := uuid.Must(uuid.FromString(strings.Split(r.URL.Path, "/")[1]))
+                        // TODO: seems like this probably isn't concurrency-safe
+                        mux := muxes[id]
+
+                        if mux != nil {
+                                requestId := nextRequestId
+                                nextRequestId++
+
+                                streamChannel := make(chan omnicore.Producer)
+                                streamChannels[requestId] = streamChannel
+
+                                done := make(chan bool)
+
+                                var msg [1]byte
+                                msg[0] = requestId
+                                mux.SendControlMessage(msg[:])
+
+                                stream := <-streamChannel
+
+                                stream.OnData(func(data []byte) {
+                                        log.Println(len(data))
+                                        w.Write(data)
+                                        stream.Request(1)
+                                })
+
+                                stream.OnEnd(func() {
+                                        log.Println("end streamy")
+                                        //mux.SendControlMessage([]byte("all done"))
+                                        done <- true
+                                })
+
+                                stream.Request(10)
+
+                                <-done
+                        } else {
+                                w.Write([]byte("invalid uuid"))
+                        }
+
+                        log.Println("exit handler")
+                } else {
+                        w.Write([]byte("Method not supported"))
+                }
+        }
+
+        http.HandleFunc("/", httpHandler)
+        http.HandleFunc("/omnistreams", muxAcceptor.GetHttpHandler())
         log.Fatal(http.ListenAndServe("localhost:9001", nil))
 }
